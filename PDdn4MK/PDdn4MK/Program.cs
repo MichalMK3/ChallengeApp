571a0e77b5d4d6868b42a01e718ff3b035f5c892
Michał Kiszakiewicz <kiszakiewiczmichalprog@outlook.com>
2023-10-04 23:56:55 +0200

    Zmienne vary i ify dzień 4

---
+++ PDdn4MK/PDdn4MK/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+string  mes1  = "Imię" ;
+Console.WriteLine(mes1) ;
+string mes2 = "Płeć" ;
+Console.WriteLine(mes2) ;
+string mes3 = "Wiek" ;
+Console.WriteLine(mes3) ;
+
+var Imię = "Ewa" ;
+var Płeć = "Kobieta" ;
+var Płeć2 = "Mężczyzna" ;
+var Wiek = 33 ;
+
+if (Imię == "Ewa" && Wiek == 33) ;
+{
+    Console.WriteLine("Ewa ,lat 33") ;
+}
+if (Płeć == "Kobieta" && Wiek < 33) 
+{
+    Console.WriteLine("Kobieta poniżej lat 30");
+}
+else if (Płeć == "Kobieta" && Wiek >= 33) 
+{
+    Console.WriteLine("Starsze panie na drugiej");
+}
+
+if (Wiek < 18 && Płeć2 == "Mężczyzna") 
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (Wiek > 18 && Płeć2 == "Mężczyzna")  
+{
+    Console.WriteLine("Pełnoletni Mężczyzna");
+}
